8caf1c6b323082a1b07be99c657c981e55402a78
RetnoNS <119503025+RetnoNS@users.noreply.github.com>
2024-12-17 16:01:15 +0700

    g

---
+++ app/src/main/java/com/example/pouchy/ForgotPassword.kt
@@ -0,0 +1,4 @@
+package com.example.pouchy
+
+class ForgotPassword {
+}
--- app/src/main/java/com/example/pouchy/MainActivity.kt
+++ app/src/main/java/com/example/pouchy/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.pouchy
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,11 +12,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(R.layout.login)
+
+        // Ambil TextView "Forgot Password"
+        val textForgotPassword: TextView = findViewById(R.id.textForgotPassword)
+
+        // Set listener untuk pindah ke halaman ForgotPasswordActivity
+        textForgotPassword.setOnClickListener {
+            val intent = Intent(this, ForgotPassword::class.java)
+            startActivity(intent)
+
+
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            }
         }
     }
 }
